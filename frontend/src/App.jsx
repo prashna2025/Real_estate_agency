fd1d700243997614c6d37b7654568b5d9c108b14
Prashna Raut <prashnaraut32@gmail.com>
2026-08-18 07:47:08 +0545

    app.jsx is updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PublicLayout from './components/public/PublicLayout';
+import AdminLayout from './components/admin/AdminLayout';
 
 // Public Pages
 import Home from './pages/public/Home';
 import Properties from './pages/public/Properties';
 import PropertyDetail from './pages/public/PropertyDetail';
+
+// Admin Pages
+import Login from './pages/admin/Login';
+import Dashboard from './pages/admin/Dashboard';
+import ManageProperties from './pages/admin/ManageProperties';
 
 function App() {
   return (
@@ -17,7 +23,16 @@ function App() {
           <Route path="property/:slug" element={<PropertyDetail />} />
         </Route>
         
-        {/* Admin Routes (Coming Day 5) */}
+        {/* Admin Auth Route */}
+        <Route path="/admin/login" element={<Login />} />
+
+        {/* Protected Admin Routes */}
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="properties" element={<ManageProperties />} />
+          {/* Create a simple ManageInquiries page similarly if needed */}
+          <Route path="inquiries" element={<div className="p-8 font-serif text-xl">Manage Inquiries (Coming soon)</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
