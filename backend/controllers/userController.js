cd9b972606fd380797081870f128ba9e71b9385f
Prashna Raut <prashnaraut32@gmail.com>
2026-08-18 13:05:12 +0545

    userController.js is edited

---
+++ backend/controllers/userController.js
@@ -0,0 +1,43 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({ name, email, password });
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email, token: generateToken(user._id) });
+  } catch (error) { res.status(400).json({ message: error.message }); }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({ _id: user._id, name: user.name, email: user.email, favorites: user.favorites, token: generateToken(user._id) });
+    } else {
+      res.status(401).json({ message: 'Invalid credentials' });
+    }
+  } catch (error) { res.status(500).json({ message: error.message }); }
+};
+
+export const toggleFavorite = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const { propertyId } = req.body;
+    
+    if (user.favorites.includes(propertyId)) {
+      user.favorites = user.favorites.filter(id => id.toString() !== propertyId);
+    } else {
+      user.favorites.push(propertyId);
+    }
+    
+    await user.save();
+    res.json(user.favorites);
+  } catch (error) { res.status(400).json({ message: error.message }); }
+};
